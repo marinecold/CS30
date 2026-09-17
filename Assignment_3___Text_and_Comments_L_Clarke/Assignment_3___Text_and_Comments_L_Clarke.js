512ece3afbb6f81a08af300b4bb3067fdc99f387
marinecold <203245@gscs.ca>
2026-09-17 09:13:18 -0600

    Assignment 3 - Text and Comments L Clarke

---
+++ Assignment_3___Text_and_Comments_L_Clarke/Assignment_3___Text_and_Comments_L_Clarke.js
@@ -0,0 +1,23 @@
+function setup() {
+  
+  //Loreno Clarke
+  //203245@gscs.ca
+  //Assignment 3 - Text and Comments
+  
+  createCanvas(250,250);
+  //Create the canvas to display the text
+
+  text("Lorenzo Clarke", 5, 20);
+  //Display first and last name
+  
+  text("Email: 203245@gscs.ca", 5, 40);
+  //Display school email
+
+  text("Grade 12 student", 5, 60);
+  //Display school grade
+}
+
+
+function draw() {
+
+}
