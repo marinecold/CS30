43e211750d58b56d798240621112b0f7c5db7d12
marinecold <203245@gscs.ca>
2026-09-18 09:42:25 -0600

    Add files via upload

---
+++ Assignment_5___Chessboard_L_Clarke/Assignment_5___Chessboard_L_Clarke.js
@@ -0,0 +1,26 @@
+//Lorenzo Clarke
+//203245@gscs.ca
+//Assignment 5 - Chessboard
+
+function setup() {
+  
+  createCanvas(200,200);
+  
+  DrawBlackSquares();
+  
+  function DrawBlackSquares(){
+    
+    for(let i = 0; i < 5; i++){
+  
+    rect(0,0,50,50);
+    
+    }
+    
+  }
+
+}
+
+
+function draw() {
+
+}
