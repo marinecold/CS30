0908476bd0308dfe21c5f97a92424282f553f505
marinecold <203245@gscs.ca>
2026-09-21 09:40:02 -0600

    Assignment,rev3

---
+++ Assignment_5___Chessboard_L_Clarke_3_/Assignment_5___Chessboard_L_Clarke_3_.js
@@ -0,0 +1,59 @@
+//Lorenzo Clarke
+//203245@gscs.ca
+//Assignment 5 - Chessboard
+
+function setup() {
+  
+  createCanvas(200,200);
+  //Creates the canvas
+  
+  noStroke();
+  //Removes the black outline around the rectangles
+  
+  DrawBlackSquares();
+  //Draws all of the black squares on the canvas
+  
+  DrawWhiteSquares();
+  //Draws all of the white squares on the canvas
+  
+  
+  function DrawWhiteSquares(){
+    
+    fill(255);
+    //Sets the infill colour of the rectangles to white
+  
+    rect(0,0,50,50);
+    rect(50,50,50,50);
+    rect(100,100,50,50);
+    rect(150,150,50,50);
+    rect(100,0,50,50);
+    rect(150,50,50,50);
+    rect(0,100,50,50);
+    rect(50,150,50,50);
+    //Creates all of the white squares based on their specific coordinates
+    
+  }
+  
+  function DrawBlackSquares(){
+    
+    fill(0);
+    //Sets the infill colour of the rectangles to black
+  
+    rect(50,0,50,50);
+    rect(150,0,50,50);
+    rect(0,50,50,50);
+    rect(100,50,50,50);
+    rect(50,100,50,50);
+    rect(150,100,50,50);
+    rect(0,150,50,50);
+    rect(100,150,50,50);
+    //Creates all of the black squares based on their specific coordinates
+    
+  }
+
+}
+
+
+function draw() {
+
+}
